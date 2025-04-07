3de57422f25e247b29dd9172d6d954656c77a8ac
Ashley Machakaire <st10443342@imconnect.edu.za>
2025-04-07 11:34:17 +0200

    part one

---
+++ cloudDevP1/Models/Venue.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.Extensions.Logging;
+
+namespace cloudDevP1.Models
+{
+    public class Venue
+    {
+        [Key]
+        public int VenueId { get; set; }
+        public string VenueName { get; set; }
+        public string Location { get; set; }
+        public int Capacity { get; set; }
+        public string ImageUrl { get; set; }
+        public List<Event> Events { get; set; } = new();
+        public List<Booking> Bookings { get; set; } = new();
+    }
+}
